231a22bafd9f6392c3c55d7c07310a7ab3c79c30
bildad58 <bildadwasioya@gmail.com>
2024-06-17 12:37:36 -0700

    future

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# define variables
+how old are you = input('enter your age)
+
+#calculate age in 2050
+age = 19
+
+#printing the age in the required year
+ print(f"in 2050, you will be {19}: years old"
