0e719f3dad0aea07ac20fab45a6cd57cd72b88fe
Bildad58 <bildadwasioya@gmail.com>
2024-06-21 21:55:29 +0300

    finance

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+x = input ("Enter your monthly income: ")
+y = input ("Enter your total monthly expenses: ")
+
+#monthlysavings
+Monthly_savings = " x - y "
+
+#Project Annual Savings
+Projected_Savings = float('Monthly_Savings' * 12 + ('Monthly_Savings' * 12 * 0.05))
+print (Projected_Savings)
