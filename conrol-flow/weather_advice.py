e9e37c784531e62da0ea626e5b21af7f559b349b
Bildad58 <bildadwasioya@gmail.com>
2024-06-26 09:57:33 +0300

    weather

--- conrol-flow/weather_advice.py
+++ conrol-flow/weather_advice.py
@@ -1,14 +1,18 @@
 Weather = input("What's the Weather like today? (sunny/rainy/cold): ")
 
+
 if Weather == "sunny" :
-    print("Wear a t-shirt and sunglasses.") 
+    print("Wear a t-shirt and sunglasses.")
+
 
 elif Weather == "rainy" :
     print("Don't forget your umbrella and a raincoat.")  
 
+
 elif Weather == "cold" :
     print("Make sure to wear a warm coat and a scar")      
 
+
 else :
-    print("Sorry, I don't have recommendations for this weather")   
- 
+    print("Sorry, I don't have recommendations for this weather")  
+
