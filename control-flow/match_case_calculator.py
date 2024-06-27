f0c85dc17af1e3a0fac52436d6192637e004dc71
Bildad58 <162963842+Bildad58@users.noreply.github.com>
2024-06-27 16:56:54 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -7,25 +7,25 @@ def main():
         num2 = (input("Enter the second number: "))
 
         # Prompt the user to choose an operation
-        operation = input("Choose the operation (+, -, *, /): ").strip()
+        operation = input("Choose the operation (+, -, *, /): ")
 
         # Perform the calculation using match-case
         match operation:
             case '+':
                 result = num1 + num2
-                print(f"The result is {result}.")
+                print("The result is {result}.")
             case '-':
                 result = num1 - num2
-                print(f"The result is {result}.")
+                print("The result is {result}.")
             case '*':
                 result = num1 * num2
-                print(f"The result is {result}.")
+                print("The result is {result}.")
             case '/':
                 if num2 == 0:
                     print("Cannot divide by zero.")
                 else:
                     result = num1 / num2
-                    print(f"The result is {result}.")
+                    print("The result is {result}.")
             case _:
                 print("Invalid operation. Please choose one of (+, -, *, /).")
     except ValueError:
