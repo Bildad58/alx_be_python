1829d1f675d7e0309f21954d7d2f63ac248a85ba
Bildad58 <162963842+Bildad58@users.noreply.github.com>
2024-07-01 00:02:17 +0300

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # prompt the use to input size of pattern
 size = int(input("Enter size of pattern:"))
 
@@ -12,17 +11,3 @@ while row<size:
      #print new line
      print("*" , end='')
     row += 1
-=======
-#prompt user to enter the following
-size = int(input("Enter size of pattern:"))
-rows = int(input("Enter number of rows:"))
-columns = int(input("Enter number of columns:"))
-symbol = input("Enter symbol to use:")
-size == rows
-# using the loops
-for x in range (rows):
-    for y in range (columns):
-        print(symbol, end='')
->>>>>>> 0dd36fee532f7c0ccc30cd161fc661f861395b23
-    print()
-~
