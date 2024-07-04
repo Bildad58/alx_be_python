5163c4e16cd8a45dc401f920fb0b928f44a9565b
Bildad58 <162963842+Bildad58@users.noreply.github.com>
2024-07-04 13:41:16 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,14 @@
 def perform_operation(num1, num2, operation ):
    
    if operation == 'add':
-      result = num1 + num2
+      return num1 + num2
    elif operation == 'divide':
       if num1 / 0:
          print("Please enter a whole number")
-      result = num1 / num2
+      return  num1 / num2
    elif operation == '*':
-      result == num1 * num2
+      return num1 * num2
    elif operation == '-':
-      result = num1 - num2
+      return  num1 - num2
    else:
       print("Invalid number and opreation")
-
-perform_operation()
