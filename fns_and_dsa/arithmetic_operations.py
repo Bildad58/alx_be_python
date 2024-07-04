18b5c0a62d566eef1aceab1616788cc12ef8ae99
Bildad58 <162963842+Bildad58@users.noreply.github.com>
2024-07-04 18:04:28 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -21,18 +21,3 @@ num2 = float(input("Enter number of choice:"))
 operation =input("Enter operation operator of choice:")
 
 perform_operation(num1, num2, operation)
-=======
-        if num2 ==  0:
-         print("Enter a whole number")
-        else:
-         return num1 / num2 
-    else:
-       print("Invalid number and operation:")
-
-num1 = float(input("Enter number of choice:"))
-num2 = float(input("Enter number of choice:"))
-operation =input("Enter operation operator of choice:")
-
-perform_operation(num1, num2, operation)
-
->>>>>>> eab572d7b3d46753448805d098cfa99e68068a93
