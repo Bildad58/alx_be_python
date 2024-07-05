2a0713d0224cc50b83970b5313b6791cc622f53a
bildad58 <bildadwasioya@gmail.com>
2024-07-05 02:26:44 -0700

    explore_datetime.py

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,17 @@
+import datetime
+def display_current_datetime():   
+    current_date = datetime.datetime(2024, 7, 5, 12, 45, 23, )
+    print(f"Current date and time: {current_date}")
+
+display_current_datetime()
+
+number_of_days = int(input("Enter number of days to add to current date:"))
+
+def calculate_future_date(YYYY, MM, DD):
+       current_date = datetime.datetime(2024, 7, 5, 12, 45, 23, )
+ 
+       time_delta = datetime.timedelta(days= {number_of_days})
+       future_date = current_date + time_delta()
+       print(f"Future date: {future_date}")
+
+calculate_future_date(2024, 7, 5)
