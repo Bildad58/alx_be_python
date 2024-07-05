be2131da4777b2ec1cde7d2ceed73bef21fbf599
Bildad58 <162963842+Bildad58@users.noreply.github.com>
2024-07-05 12:37:09 +0300

    Update explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
 from datetime import datetime , timedelta
 
 def display_current_datetime():   
-    current_date = datetime.now( )
+    current_date = datetime.now()
     print(f"Current date and time: {current_date}")
 
 display_current_datetime()
