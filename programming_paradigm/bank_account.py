38b37253f5b664f0af01888be765b5009337961e
Bildad58 <bildadwasioya@gmail.com>
2024-07-09 17:56:27 +0300

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,9 +1,9 @@
-class BankAccount:
+
     def __init__(self, account_balance, initial_balance=0):
         self.account_balance = account_balance
         self.initial_balance = initial_balance
-    
-        
+
+
     def deposit(self,amount):
         if amount > 0:
             self.account_balance = amount + self.account_balance
@@ -20,6 +20,4 @@ class BankAccount:
         else:
             print("Insufficient funds")
 
-    def display_balance(self):
-        print(f"Current Balance {self.account_balance}")
 
