b6f8ec4bbf6fc2ce8e8d127ba5bce9f7b6617724
Bildad58 <162963842+Bildad58@users.noreply.github.com>
2024-07-09 20:02:58 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,7 +14,7 @@ class BankAccount:
             self.account_balance -= amount 
             return f"Withdrew: ${amount}.0" 
         else:
-            return Insufficient funds.
+            return "Insufficient funds."
         
 
     def display_balance(self):
