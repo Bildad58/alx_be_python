e2810b6e9fdad38d1961db8df4a83b27eceeeb0a
Bildad58 <162963842+Bildad58@users.noreply.github.com>
2024-07-09 18:17:47 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,7 +7,7 @@ class BankAccount:
     def deposit(self,amount):
         if amount > 0:
             self.account_balance = amount + self.account_balance
-            return f"Deposited: {amount}"
+            return self.account_balance
         else:
             print(f"{amount}, should be greater than {self.initial_balance}")
 
@@ -20,7 +20,3 @@ class BankAccount:
 
     def display_balance(self):
         print(f"Current Balance: {self.account_balance}")
-<<<<<<< HEAD
-=======
-
->>>>>>> 554f3f2b1905fe0d379d1eb02cac40db603b5b9a
