41b1ceccce11fd5c562b67237b8999a03c13a886
Bildad58 <162963842+Bildad58@users.noreply.github.com>
2024-07-09 19:15:36 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,7 +7,7 @@ class BankAccount:
     def deposit(self,amount):
         if amount > 0:
             self.account_balance = amount + self.account_balance
-            return self.account_balance
+            return f"Withdrew: ${amount}.0"
         else:
             print(f"{amount}, should be greater than {self.initial_balance}"
 
