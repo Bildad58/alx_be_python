d3af763ad1a81defe97101726659c6d3eab45955
Bildad58 <bildadwasioya@gmail.com>
2024-07-09 14:24:36 +0300

    bank

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, account_balance, initial_balance=0):
+        self.account_balance = account_balance
+        self.initial_balance = initial_balance
+    
+        
+    def deposit(self,amount):
+        if amount > 0:
+            self.account_balance = amount + self.account_balance
+            return self.account_balance
+        else:
+            print(f"{amount}, should be greater than {self.initial_balance}")
+
+    def withdraw(self,amount):
+        if amount > self.account_balance:
+            self.account_balance -= self.account_balance
+            return 
+        else:
+            print(f"{amount}, should be less than {self.account_balance}")
+
+    def display_balance(self):
+        print(f"Your account balance is {self.account_balance}")
