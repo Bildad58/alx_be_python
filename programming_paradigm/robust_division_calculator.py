f8ec02421536fc1f05ba5fb108e539620cd39086
Bildad58 <162963842+Bildad58@users.noreply.github.com>
2024-07-10 15:08:10 +0300

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -12,7 +12,7 @@ def safe_divide(numerator, denominator):
         print("Error: Please enter numeric values only.")
     
     else:
-        print(f"The result of the division is {result}")
+        print(f"The result of the division is{result}")
 
 
 safe_divide(12,2)
