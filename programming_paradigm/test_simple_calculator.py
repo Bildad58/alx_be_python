02ab78841c5e37f1a617c92f3e160b2ffe6c3e30
Bildad58 <bildadwasioya@gmail.com>
2024-07-11 11:04:17 +0300

    test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,18 +13,18 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(9, 9), 19)
         # Add more assertions to thoroughly test the add method.
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(7, 2), 5)
         self.assertEqual(self.calc.subtract(12, 2), 10)
         self.assertEqual(self.calc.subtract(20, 8), 12)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertEqual(self.calc.divide(24, 3), 8)
         self.assertEqual(self.calc.divide(90, 0), 9)
         self.assertEqual(self.calc.divide(12, 4), 3)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3, 4), 2)
         self.assertEqual(self.calc.multiply(9, 4), 36)
         self.assertEqual(self.calc.multiply(990, 48), 990 * 48)
