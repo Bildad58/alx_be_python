1664bcf8453633624129f4df7e43b0820d23ee8e
Bildad58 <bildadwasioya@gmail.com>
2024-07-11 12:57:24 +0300

    library_management.py

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,26 @@
+class Book:
+   def __init__(self,title, author):
+      self.title = title
+      self.author = author
+      self.is_checked_out = True
+
+class Library:
+   def __init__(self):
+      self._books = (self)
+
+   def add_book(self,book):
+      self.book = [book]
+
+   def check_out_book(self,title):
+      if self.check_out_book:
+         self.check_out_book = title
+         return True
+      return False
+
+   def return_book(self,title):
+    if self.return_book:
+       self._books = title
+       return
+      
+   def list_available_books(self):
+      self._books =[]
