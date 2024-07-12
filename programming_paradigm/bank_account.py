fdcf5dadb515f710a99276d0163083e148df620f
Bildad58 <bildadwasioya@gmail.com>
2024-07-12 16:27:13 +0300

    update

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,7 +14,7 @@ class BankAccount:
     def withdraw(self,amount):
         if amount > self.account_balance:
             self.account_balance -= amount
-            return f"Withdrew: {amz}"
+            return f"Withdrew: {amount}"
         else:
             print(f"{amount}, should be less than {self.account_balance}")
 
